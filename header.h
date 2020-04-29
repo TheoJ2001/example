310dfaade20b2bcea0cf9da48b949092552fe224
Theo Johansson <theo.johansson.ga@gmail.com>
2020-04-29 14:17:51 +0200

    Added feature
    a simple "hello world" program with a header file

---
+++ header.h
@@ -0,0 +1,6 @@
+#ifndef HEADER_H
+#define HEADER
+
+string message = "Hello World";
+
+#endif
---
+++ main.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+#include <header.h>
+using namespace std;
+
+int main(){
+    cout << message;
